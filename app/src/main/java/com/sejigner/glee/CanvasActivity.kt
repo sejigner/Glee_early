e7886ba257957c89014ea358c742d6c6e3b23335
Sejin Choi <sejigner@gmail.com>
2021-07-04 23:34:16 +0900

    Implement PaintView

---
+++ app/src/main/java/com/sejigner/glee/CanvasActivity.kt
@@ -0,0 +1,83 @@
+package com.sejigner.glee
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewTreeObserver
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.doOnLayout
+import kotlinx.android.synthetic.main.activity_canvas.*
+import kotlinx.android.synthetic.main.activity_canvas.tv_hambaksnow
+import kotlinx.android.synthetic.main.activity_canvas.tv_cafe24
+import kotlinx.android.synthetic.main.activity_canvas.tv_arita
+import kotlinx.android.synthetic.main.activity_canvas.tv_mapo
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class CanvasActivity : AppCompatActivity() {
+    private var paintView: PaintView? = null
+    private var width : Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_canvas)
+        paintView = findViewById<View>(R.id.paintView) as PaintView
+        val metrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(metrics)
+        paintView!!.doOnLayout {
+            var width = it.measuredWidth
+            var height = it.measuredHeight
+            paintView!!.init(width, height)
+        }
+
+        tv_cafe24.setOnClickListener {
+            tv_work_canvas.typeface = Typeface.createFromAsset(applicationContext.assets, "fonts/cafe24_surround_air.ttf")
+        }
+
+        tv_arita.setOnClickListener {
+            tv_work_canvas.typeface = Typeface.createFromAsset(applicationContext.assets, "fonts/arita_buri.otf")
+        }
+
+        tv_mapo.setOnClickListener {
+            tv_work_canvas.typeface = Typeface.createFromAsset(applicationContext.assets, "fonts/mapo_flower_island.ttf")
+        }
+
+        tv_hambaksnow.setOnClickListener {
+            tv_work_canvas.typeface = Typeface.createFromAsset(applicationContext.assets, "fonts/hambaksnow.ttf")
+        }
+
+        Toast.makeText(this,"원하는 글씨체를 선택하고 따라써보세요.", Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.normal -> {
+                paintView!!.normal()
+                return true
+            }
+            R.id.emboss -> {
+                paintView!!.emboss()
+                return true
+            }
+            R.id.blur -> {
+                paintView!!.blur()
+                return true
+            }
+            R.id.clear -> {
+                paintView!!.clear()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
--- app/src/main/java/com/sejigner/glee/fragment/FragmentHome.kt
+++ app/src/main/java/com/sejigner/glee/fragment/FragmentHome.kt
@@ -1,15 +1,59 @@
 package com.sejigner.glee.fragment
 
+import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.sejigner.glee.CanvasActivity
 import com.sejigner.glee.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class FragmentHome : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
+
+    private lateinit var tvCafe24: TextView
+    private lateinit var tvAritaBuri: TextView
+    private lateinit var tvMapoFlowerIsland: TextView
+    private lateinit var tvHambakSnow: TextView
+    private lateinit var tvNewTranscription: TextView
+
+   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
             : View? {
+
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvCafe24 = view.findViewById(R.id.tv_cafe24)
+        tvAritaBuri = view.findViewById(R.id.tv_arita)
+        tvMapoFlowerIsland = view.findViewById(R.id.tv_mapo)
+        tvHambakSnow = view.findViewById(R.id.tv_hambaksnow)
+        tvNewTranscription = view.findViewById(R.id.tv_new_transcription)
+
+        tvCafe24.setOnClickListener {
+            tv_work_example.typeface = Typeface.createFromAsset(requireActivity().assets, "fonts/cafe24_surround_air.ttf")
+        }
+        tvAritaBuri.setOnClickListener {
+            tv_work_example.typeface = Typeface.createFromAsset(requireActivity().assets, "fonts/arita_buri.otf")
+        }
+        tvMapoFlowerIsland.setOnClickListener {
+            tv_work_example.typeface = Typeface.createFromAsset(requireActivity().assets, "fonts/mapo_flower_island.ttf")
+        }
+
+        tvHambakSnow.setOnClickListener {
+            tv_work_example.typeface = Typeface.createFromAsset(requireActivity().assets, "fonts/hambaksnow.ttf")
+        }
+
+        tvNewTranscription.setOnClickListener {
+            val intent = Intent(activity, CanvasActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
