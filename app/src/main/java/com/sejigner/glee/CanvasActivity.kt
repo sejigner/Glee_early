dd4bbff2905b6ffac6fa04131f914f83d907dea9
sejigner <sejigner@gmail.com>
2021-07-10 17:50:10 +0900

    implement undo and redo features

--- app/src/main/java/com/sejigner/glee/CanvasActivity.kt
+++ app/src/main/java/com/sejigner/glee/CanvasActivity.kt
@@ -3,50 +3,51 @@ package com.sejigner.glee
 import android.graphics.Typeface
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewTreeObserver
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.doOnLayout
 import com.sejigner.glee.Scroll.isPainting
 import kotlinx.android.synthetic.main.activity_canvas.*
-import kotlinx.android.synthetic.main.activity_canvas.tv_hambaksnow
-import kotlinx.android.synthetic.main.activity_canvas.tv_cafe24
 import kotlinx.android.synthetic.main.activity_canvas.tv_arita
+import kotlinx.android.synthetic.main.activity_canvas.tv_cafe24
+import kotlinx.android.synthetic.main.activity_canvas.tv_hambaksnow
 import kotlinx.android.synthetic.main.activity_canvas.tv_mapo
 import kotlinx.android.synthetic.main.fragment_home.*
 
 object Scroll {
-    var isPainting:Boolean = true
+    var isPainting: Boolean = true
 }
 
 class CanvasActivity : AppCompatActivity() {
-    private var paintView: PaintView? = null
-    private var width : Int? = null
+    private var customView: CustomView? = null
+    private var width: Int? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_canvas)
-        paintView = findViewById<View>(R.id.paintView) as PaintView
+        customView = findViewById<View>(R.id.customView) as CustomView
         val metrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(metrics)
-        paintView!!.doOnLayout {
+        /*
+        customView!!.doOnLayout {
             var width = it.measuredWidth
             var height = it.measuredHeight
-            paintView!!.init(width, height)
+            customView!!.init(width, height)
         }
+        */
         btn_toggle_scroll.setOnClickListener {
-            if(isPainting) {
-                paintView!!.setOnTouchListener { view, event -> // 터치 이벤트 제거 (필사 기능 off)
+            if (isPainting) {
+                customView!!.setOnTouchListener { view, event -> // 터치 이벤트 제거 (필사 기능 off)
                     true
                 }
                 isPainting = false
-            }
-            else {
-                paintView!!.setOnTouchListener { view, event -> // 터치 이벤트
+            } else {
+                customView!!.setOnTouchListener { view, event -> // 터치 이벤트
                     false
                 }
                 isPainting = true
@@ -71,35 +72,15 @@ class CanvasActivity : AppCompatActivity() {
             tv_work_canvas.typeface = Typeface.createFromAsset(applicationContext.assets, "fonts/hambaksnow.ttf")
         }
 
-        Toast.makeText(this,"원하는 글씨체를 선택하고 따라써보세요.", Toast.LENGTH_LONG).show()
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val menuInflater = menuInflater
-        menuInflater.inflate(R.menu.main, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.normal -> {
-                paintView!!.normal()
-                return true
-            }
-            R.id.emboss -> {
-                paintView!!.emboss()
-                return true
-            }
-            R.id.blur -> {
-                paintView!!.blur()
-                return true
-            }
-            R.id.clear -> {
-                paintView!!.clear()
-                return true
-            }
+        btn_undo.setOnClickListener {
+            customView!!.onClickUndo()
         }
-        return super.onOptionsItemSelected(item)
+
+        btn_redo.setOnClickListener {
+            customView!!.onClickRedo()
+        }
+
+        Toast.makeText(this, "원하는 글씨체를 선택하고 따라써보세요.", Toast.LENGTH_LONG).show()
+
     }
 }
---
+++ app/src/main/java/com/sejigner/glee/CustomView.java
@@ -0,0 +1,141 @@
+package com.sejigner.glee;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+
+public class CustomView extends View {
+    private static final float TOUCH_TOLERANCE = 4;
+    private float mX, mY;
+    private Path drawPath;
+    private Paint canvasPaint;
+    public static final int DEFAULT_BG_COLOR = Color.TRANSPARENT;
+    private Paint drawPaint;
+    private int paintColor = 0xFF00FF0C;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+    private float currentBrushSize, lastBrushSize;
+    private ArrayList<Path> paths = new ArrayList<Path>();
+    private ArrayList<Path> undonePaths = new ArrayList<Path>();
+    private int backgroundColor = DEFAULT_BG_COLOR;
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        currentBrushSize = getResources().getInteger(R.integer.medium_size);
+        lastBrushSize = currentBrushSize;
+
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(currentBrushSize);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        getParent().requestDisallowInterceptTouchEvent(true);
+        for (Path p : paths) {
+            canvas.drawPath(p, drawPaint);
+        }
+        canvas.drawPath(drawPath, drawPaint);
+
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        // create canvas of certain device size.
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        // create Bitmap of certain w, h
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+
+        // apply bitmap to graphic to start drawing.
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        // respond to down, move and up events
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touch_start(touchX, touchY);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touch_move(touchX, touchY);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touch_up();
+                invalidate();
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    private void touch_up() {
+        drawPath.lineTo(mX, mY);
+        drawCanvas.drawPath(drawPath, drawPaint);
+        paths.add(drawPath);
+        drawPath = new Path();
+    }
+
+    private void touch_move(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            drawPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    private void touch_start(float x, float y) {
+        undonePaths.clear();
+        drawPath.reset();
+        drawPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    public void onClickUndo() {
+        if (paths.size() > 0) {
+            undonePaths.add(paths.remove(paths.size() - 1));
+            invalidate();
+        }
+    }
+
+    public void onClickRedo() {
+        if (undonePaths.size() > 0) {
+            paths.add(undonePaths.remove(undonePaths.size() - 1));
+            invalidate();
+        }
+    }
+
+
+}
