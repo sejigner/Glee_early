f7aa275a355781a1603133028e5d62b581ac54ae
sejigner <sejigner@gmail.com>
2021-07-16 12:15:08 +0900

    make work-detail-activity

---
+++ app/src/main/java/com/sejigner/glee/WorkDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.sejigner.glee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_detail)
+    }
+}
